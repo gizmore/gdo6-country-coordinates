dc2f8847497a719083671dda4d9f8a3210be32cc
gizmore <gizmore@wechall.net>
2018-08-15 23:38:06 +0200

    6.08-r7-code-formatting

--- Method/Detect.php
+++ Method/Detect.php
@@ -12,78 +12,78 @@ use GDO\Country\GDO_Country;
  */
 final class Detect extends Method
 {
-    public function getPermission()
-    {
-        return 'staff';
-    }
-    
-    public function execute()
-    {
-        if (!($country = $this->detect(Common::getGetFloat('lat'), Common::getGetFloat('lng'))))
-        {
-            $country = GDO_Country::unknownCountry();
-        }
-        die(json_encode($country->toJSON()));
-    }
-        
-    public function detect($lat=null, $lng=null)
-    {
-        $probable = GDO_CountryCoordinates::probableCountries($lat, $lng);
-        foreach ($probable as $country)
-        {
-            $geometry = GDO_CountryCoordinates::loadGeometry($country);
-            if ($this->insideGeometry($lat, $lng, $geometry))
-            {
-                return $country;
-            }
-        }
-        return GDO_Country::unknownCountry();
-    }
-    
-    private function insideGeometry($lat=null, $lng=null, $geometry)
-    {
-        switch ($geometry->type)
-        {
-            case 'None':
-                return false;
-            case 'Polygon':
-                foreach ($geometry->coordinates as $coords)
-                {
-                    if (self::insidePolygon($lat, $lng, $coords))
-                    {
-                        return true;
-                    }
-                }
-                break;
-            case 'MultiPolygon':
-                foreach ($geometry->coordinates as $polygon)
-                {
-                    foreach ($polygon as $coords)
-                    {
-                        if (self::insidePolygon($lat, $lng, $coords))
-                        {
-                            return true;
-                        }
-                    }
-                }
-                break;
-        }
-        return false;
-    }
-    
-    private function insidePolygon($lat=null, $lng=null, $coords)
-    {
-       $result = false;
-       $nvert = count($coords);
-       for ($i = 0, $j = $nvert-1; $i < $nvert; $j = $i++) {
-           if ( (($coords[$i][1]>$lat) != ($coords[$j][1]>$lat)) &&
-               ($lng < (($coords[$j][0]-$coords[$i][0]) * ($lat-$coords[$i][1]) / ($coords[$j][1] - $coords[$i][1]) + $coords[$i][0])) )
-           {
-               $result = !$result;
-           }
-        }
-        return $result;
-    }
-        
-    
+	public function getPermission()
+	{
+		return 'staff';
+	}
+	
+	public function execute()
+	{
+		if (!($country = $this->detect(Common::getGetFloat('lat'), Common::getGetFloat('lng'))))
+		{
+			$country = GDO_Country::unknownCountry();
+		}
+		die(json_encode($country->toJSON()));
+	}
+		
+	public function detect($lat=null, $lng=null)
+	{
+		$probable = GDO_CountryCoordinates::probableCountries($lat, $lng);
+		foreach ($probable as $country)
+		{
+			$geometry = GDO_CountryCoordinates::loadGeometry($country);
+			if ($this->insideGeometry($lat, $lng, $geometry))
+			{
+				return $country;
+			}
+		}
+		return GDO_Country::unknownCountry();
+	}
+	
+	private function insideGeometry($lat=null, $lng=null, $geometry)
+	{
+		switch ($geometry->type)
+		{
+			case 'None':
+				return false;
+			case 'Polygon':
+				foreach ($geometry->coordinates as $coords)
+				{
+					if (self::insidePolygon($lat, $lng, $coords))
+					{
+						return true;
+					}
+				}
+				break;
+			case 'MultiPolygon':
+				foreach ($geometry->coordinates as $polygon)
+				{
+					foreach ($polygon as $coords)
+					{
+						if (self::insidePolygon($lat, $lng, $coords))
+						{
+							return true;
+						}
+					}
+				}
+				break;
+		}
+		return false;
+	}
+	
+	private function insidePolygon($lat=null, $lng=null, $coords)
+	{
+	   $result = false;
+	   $nvert = count($coords);
+	   for ($i = 0, $j = $nvert-1; $i < $nvert; $j = $i++) {
+		   if ( (($coords[$i][1]>$lat) != ($coords[$j][1]>$lat)) &&
+			   ($lng < (($coords[$j][0]-$coords[$i][0]) * ($lat-$coords[$i][1]) / ($coords[$j][1] - $coords[$i][1]) + $coords[$i][0])) )
+		   {
+			   $result = !$result;
+		   }
+		}
+		return $result;
+	}
+		
+	
 }
--- Module_CountryCoordinates.php
+++ Module_CountryCoordinates.php
@@ -4,15 +4,15 @@ use GDO\Core\GDO_Module;
 
 final class Module_CountryCoordinates extends GDO_Module
 {
-    public $module_priority = 8;
-    
-    public function onInstall() { InstallGeocountries::install(); }
+	public $module_priority = 8;
+	
+	public function onInstall() { InstallGeocountries::install(); }
 
-    public function getClasses()
-    {
-        return array(
-            'GDO\CountryCoordinates\GDO_CountryCoordinates'
-        );
-    }
-    
+	public function getClasses()
+	{
+		return array(
+			'GDO\CountryCoordinates\GDO_CountryCoordinates'
+		);
+	}
+	
 }
